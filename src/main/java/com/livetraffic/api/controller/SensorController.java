b2447282d43d4d2a0ae91ab5030e42706e1a3286
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2023-07-01 15:54:58 -0300

    add WebFlux stuff & SensorController, update props

---
+++ src/main/java/com/livetraffic/api/controller/SensorController.java
@@ -0,0 +1,31 @@
+package com.livetraffic.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.MediaType;
+
+import reactor.core.publisher.Flux;
+
+//import com.livetraffic.api.services.SensorService;
+
+@RestController
+@RequestMapping("/sensors")
+public class SensorController {
+
+	//@Autowired
+	//private SensorService service;
+
+	@GetMapping
+	public Flux<String> getAll() {
+		return null;
+	}
+
+	@GetMapping(path = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> subscribe() {
+		return null;
+	}
+
+}
